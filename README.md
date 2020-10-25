674a394a7842f962eaee692aa344c8f5aba66d0b
pyanush <dhiman.anushka23@gmail.com>
2020-10-25 17:50:38 +0530

    Add all my files

---
+++ README.md
@@ -0,0 +1,40 @@
+# ObjectTracking-DeepSORT-YOLOv3-TF2
+This repository implements YOLOv3 and Deep SORT in order to perfrom real-time object tracking. 
+
+## Installation
+
+First, clone or download this GitHub repository. Install requirements and download pretrained weights:
+
+```
+https://github.com/anushkadhiman/ObjectTracking-DeepSORT-YOLOv3-TF2.git
+cd ObjectTracking-DeepSORT-YOLOv3-TF2
+````
+
+```
+pip install -r ./requirements.txt
+`````
+
+```
+# yolov3
+wget -P model_data https://pjreddie.com/media/files/yolov3.weights
+
+# yolov3-tiny
+wget -P model_data https://pjreddie.com/media/files/yolov3-tiny.weights
+``````
+
+
+## Tracking
+
+```
+python object_tracker.py
+````
+
+## Result
+
+### Tracking Person by Pre-trained model
+![Alt text](tracking.gif?raw=true "video")
+
+
+
+
+
---
+++ yolov3/configs.py
@@ -0,0 +1,53 @@
+# YOLO options
+YOLO_TYPE                   = "yolov3"
+YOLO_FRAMEWORK              = "tf" # "tf" or "trt"
+YOLO_V3_WEIGHTS             = "model_data/yolov3.weights"
+YOLO_V3_TINY_WEIGHTS        = "model_data/yolov3-tiny.weights"
+YOLO_TRT_QUANTIZE_MODE      = "INT8" # INT8, FP16, FP32
+YOLO_CUSTOM_WEIGHTS         = False # "checkpoints/yolov3_custom" # used in evaluate_mAP.py and custom model detection, if not using leave False
+                            # YOLO_CUSTOM_WEIGHTS also used with TensorRT and custom model detection
+YOLO_COCO_CLASSES           = "model_data/coco/coco.names"
+YOLO_STRIDES                = [8, 16, 32]
+YOLO_IOU_LOSS_THRESH        = 0.5
+YOLO_ANCHOR_PER_SCALE       = 3
+YOLO_MAX_BBOX_PER_SCALE     = 100
+YOLO_INPUT_SIZE             = 416
+YOLO_ANCHORS            = [[[10,  13], [16,   30], [33,   23]],
+                           [[30,  61], [62,   45], [59,  119]],
+                           [[116, 90], [156, 198], [373, 326]]]
+# Train options
+TRAIN_YOLO_TINY             = False
+TRAIN_SAVE_BEST_ONLY        = True # saves only best model according validation loss (True recommended)
+TRAIN_SAVE_CHECKPOINT       = False # saves all best validated checkpoints in training process (may require a lot disk space) (False recommended)
+TRAIN_CLASSES               = "model_data/custom_data.names"
+TRAIN_ANNOT_PATH            = "model_data/custom_data_train.txt"
+TRAIN_LOGDIR                = "log"
+TRAIN_CHECKPOINTS_FOLDER    = "checkpoints"
+TRAIN_MODEL_NAME            = f"{YOLO_TYPE}_custom"
+TRAIN_LOAD_IMAGES_TO_RAM    = True # With True faster training, but need more RAM
+TRAIN_BATCH_SIZE            = 4
+TRAIN_INPUT_SIZE            = 416
+TRAIN_DATA_AUG              = True
+TRAIN_TRANSFER              = True
+TRAIN_FROM_CHECKPOINT       = False # "checkpoints/yolov3_custom"
+TRAIN_LR_INIT               = 1e-4
+TRAIN_LR_END                = 1e-6
+TRAIN_WARMUP_EPOCHS         = 2
+TRAIN_EPOCHS                = 100
+
+# TEST options
+TEST_ANNOT_PATH             = "model_data/custom_data_test.txt"
+TEST_BATCH_SIZE             = 4
+TEST_INPUT_SIZE             = 416
+TEST_DATA_AUG               = False
+TEST_DECTECTED_IMAGE_PATH   = ""
+TEST_SCORE_THRESHOLD        = 0.3
+TEST_IOU_THRESHOLD          = 0.45
+
+
+#YOLOv3-TINY WORKAROUND
+if TRAIN_YOLO_TINY:
+    YOLO_STRIDES            = [16, 32, 64]    
+    YOLO_ANCHORS            = [[[10,  14], [23,   27], [37,   58]],
+                               [[81,  82], [135, 169], [344, 319]],
+                               [[0,    0], [0,     0], [0,     0]]]
